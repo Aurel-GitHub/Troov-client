959154fb46e2564df279c1d94def1377ec3b6c77
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2022-07-12 00:28:17 +0200

    add btn update & delete with restriction

--- components/layouts/ItemCard.vue
+++ components/layouts/ItemCard.vue
@@ -7,12 +7,18 @@
     img-width="100%"
     img-top
     tag="article"
-    style="max-width: 20rem; padding-top: 1rem"
-    class="col-md-5 my-3 mx-4"
+    style="max-width: 16rem; width: 16rem; padding-top: 1rem"
+    class="col-md-5 my-3 mx-5"
   >
     <b-card-text style="max-width: 10rem; height: 8rem; max-height: 8rem">
       {{ item.description }}
     </b-card-text>
+    <div v-if="isUserAccess()" class="d-flex flex-col justify-content-between">
+      <b-button size="sm" variant="outline-primary" @click="update(item._id)"
+        >Modifier</b-button
+      >
+      <b-button size="sm" variant="danger">Supprimer</b-button>
+    </div>
   </b-card>
 </template>
 
@@ -32,8 +38,15 @@ export default {
     };
   },
   methods: {
-    detail(itemId) {
+    update(itemId) {
       this.$router.push(`/item/${itemId}`);
+    },
+    isUserAccess() {
+      if (this.$store.state.user) {
+        return this.$store.state.user.userId === this.item.userId;
+      } else {
+        return false;
+      }
     },
   },
 };
---
+++ pages/item/_id.vue
@@ -0,0 +1,7 @@
+<template><h4>qsdqsdqsdqsd</h4></template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
